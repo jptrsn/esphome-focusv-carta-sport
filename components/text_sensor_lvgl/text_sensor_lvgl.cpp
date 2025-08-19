570e126266ef3fe685273dd6035ea1eeeb5c6512
James Petersen <ptrsnja@gmail.com>
2025-08-18 23:32:31 -0400

    lvgl text sensor schema

--- components/text_sensor_lvgl/text_sensor_lvgl.cpp
+++ components/text_sensor_lvgl/text_sensor_lvgl.cpp
@@ -3,12 +3,42 @@
 namespace esphome {
 namespace text_sensor_lvgl {
 
-class TextSensorLVGLPlatform : public text_sensor::TextSensor {
+using namespace esphome;
+
+class TextSensorLVGLComponent : public TextSensorLVGL, public Component {
  public:
-  TextSensorLVGLPlatform(TextSensorLVGL *sensor, lv_obj_t* widget) {
-    sensor->set_widget(widget);
+  TextSensorLVGLComponent() {}
+
+  void setup() override {
+    // ESPHome will call this after construction
   }
 };
+
+// -----------------------------
+// Platform registration
+// -----------------------------
+#include "esphome/core/defines.h"
+#include "esphome/core/application.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+
+TextSensorLVGL *make_text_sensor_lvgl(lv_obj_t *widget) {
+  auto sensor = new TextSensorLVGL();
+  sensor->set_widget(widget);
+  App.register_component(sensor);
+  App.register_text_sensor(sensor);
+  return sensor;
+}
+
+// YAML config schema
+static const text_sensor::TextSensorSchema CONFIG_SCHEMA = text_sensor::text_sensor_schema_base();
+
+void register_text_sensor_lvgl(const std::string &widget_id,
+                               text_sensor::TextSensor *sensor) {
+  lv_obj_t *widget = id(widget_id);
+  if (widget) {
+    dynamic_cast<TextSensorLVGL *>(sensor)->set_widget(widget);
+  }
+}
 
 }  // namespace text_sensor_lvgl
 }  // namespace esphome
--- components/text_sensor_lvgl/text_sensor_lvgl.h
+++ components/text_sensor_lvgl/text_sensor_lvgl.h
@@ -2,16 +2,18 @@
 #include "esphome.h"
 #include "lvgl.h"
 
+namespace esphome {
+namespace text_sensor_lvgl {
+
 using namespace esphome;
 
-class TextSensorLVGL : public text_sensor::TextSensor, public Component {
+class TextSensorLVGL : public text_sensor::TextSensor {
  public:
   lv_obj_t* widget = nullptr;
 
   void set_widget(lv_obj_t* w) { widget = w; }
 
   void publish_state(const std::string &state) override {
-    // Update LVGL widget
     if (widget) {
       const char* type = lv_obj_get_type(widget);
       if (type && strstr(type, "label")) {
@@ -21,8 +23,9 @@ class TextSensorLVGL : public text_sensor::TextSensor, public Component {
       }
       lv_obj_invalidate(widget);
     }
-
-    // Normal text sensor publish
     text_sensor::TextSensor::publish_state(state);
   }
 };
+
+}  // namespace text_sensor_lvgl
+}  // namespace esphome
